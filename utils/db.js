bbc800362cbab9b78043792e69e6139de1dd341a
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-01-09 00:14:28 +0100

    db.js conforms to linter rules

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,9 @@
 /** Exports a pooled connection to the MySQL database  */
-var mysql = require("mysql");
+const mysql = require('mysql');
 
-const credentials = require("../config/keys").mySQL;
+const credentials = require('../config/keys').mySQL;
 
-var db_pool = mysql.createPool(credentials);
+const dbPool = mysql.createPool(credentials);
 
 /**
  * @desc A helper function for handling DB connections to the SQL database
@@ -11,26 +11,25 @@ var db_pool = mysql.createPool(credentials);
  * @param {*} callback - Callback function - Pass in 'err' and 'rows'
  */
 module.exports = (req, callback) => {
-  db_pool.getConnection((err, conn) => {
+  dbPool.getConnection((err, conn) => {
     if (err) {
-      callback({ code: 100, status: "Error in connection database" }, null);
+      callback({ code: 100, status: 'Error in connection database' }, null);
       return;
     }
 
-    console.log("MYSQL: Connected as id " + conn.threadId);
+    console.log(`MYSQL: Connected as id ${conn.threadId}`);
 
-    console.log("MYSQL QUERY: %s", req);
+    console.log('MYSQL QUERY: %s', req);
 
-    conn.query(req, (err, rows) => {
+    conn.query(req, (_err, rows) => {
       conn.release();
-      if (!err) {
+      if (!_err) {
         callback(null, rows);
       }
     });
 
-    conn.on("error", function(err) {
-      callback({ code: 100, status: "Error in connection database" }, null);
-      return;
+    conn.on('error', () => {
+      callback({ code: 100, status: 'Error in connection database' }, null);
     });
   });
 };
