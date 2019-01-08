69207ea9919172f44b945532505d26eee61e0393
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-01-08 20:00:45 +0100

    server.js conforms to linting rules

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Get the server app
-const app = require("./src/app");
+const app = require('./src/app');
 
 // Server Setup
-const PORT = require("./config/keys").apiPort;
+const PORT = require('./config/keys').apiPort;
 
-console.log(":: Server Environment: %s", process.env.NODE_ENV);
+console.log(':: Server Environment: %s', process.env.NODE_ENV);
 // Start the server...
-app.listen(PORT, () => console.log("--> Server started on port %s", PORT));
+app.listen(PORT, () => console.log('--> Server started on port %s', PORT));
