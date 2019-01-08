9e07ee3cca354637f09e564945cfdeb878ec07e1
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-01-08 20:49:06 +0100

    Server.js will no longer print undefined for missing env.NODE_ENV

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = require('./src/app');
 // Server Setup
 const PORT = require('./config/keys').apiPort;
 
-console.log(':: Server Environment: %s', process.env.NODE_ENV);
+const processEnv = process.env.NODE_ENV || 'DEVELOPMENT';
+
+console.log(':: Server Environment: %s', processEnv);
 // Start the server...
 app.listen(PORT, () => console.log('--> Server started on port %s', PORT));
