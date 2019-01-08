b5c1a572baf9d98b01e1f46bfb32df87a66a786a
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-01-08 20:06:57 +0100

    app.js conforms to linting rules

--- src/app.js
+++ src/app.js
@@ -1,23 +1,20 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
-const path = require("path");
-
-// Server Setup
-const PORT = require("../config/keys").apiPort;
+const path = require('path');
 
 // Routes
-const users = require("../routes/api/users");
-const profile = require("../routes/api/profile");
-const posts = require("../routes/api/posts");
-const notams = require("../routes/api/notams");
+const users = require('../routes/api/users');
+const profile = require('../routes/api/profile');
+const posts = require('../routes/api/posts');
+const notams = require('../routes/api/notams');
 
-const events = require("../routes/api/events");
+const events = require('../routes/api/events');
 
-const docs = require("../routes/docs/docs");
-const statics = require("../routes/statics/statics");
+const docs = require('../routes/docs/docs');
+const statics = require('../routes/statics/statics');
 
 // Initialize the server
 const app = express();
@@ -27,42 +24,45 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("../config/keys").mongoURI;
+const db = require('../config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db, {
-    // Options
-    useNewUrlParser: true
-  })
-  .catch(err => console.log("!!! %s", err));
+  .connect(
+    db,
+    {
+      // Options
+      useNewUrlParser: true,
+    },
+  )
+  .catch((err) => console.log('!!! %s', err));
 
 // Passport middleware
 app.use(passport.initialize());
 
 // Passport Configs
-require("../config/passport")(passport);
+require('../config/passport')(passport);
 
 // Use API Routes
-app.use("/api/v1/users", users);
-app.use("/api/v1/profile", profile);
-app.use("/api/v1/posts", posts);
-app.use("/api/v1/notams", notams);
+app.use('/api/v1/users', users);
+app.use('/api/v1/profile', profile);
+app.use('/api/v1/posts', posts);
+app.use('/api/v1/notams', notams);
 
-app.use("/api/v1/events", events);
+app.use('/api/v1/events', events);
 
 // Use route for Documentation
-app.use("/docs/", docs);
+app.use('/docs/', docs);
 
 // Use route for Static page-content
-app.use("/statics/", statics);
+app.use('/statics/', statics);
 
 // If we are in PRODUCTION - then we want to server our client build folder for all non-api calls
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
