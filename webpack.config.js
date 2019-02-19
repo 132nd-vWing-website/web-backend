1093f406f7a33dc591f52166efde628eca1d69cd
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-02-19 20:22:48 +0100

    trying to prevent building in development mode

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 // const versionString = childProcess.execSync('git rev-list HEAD --count').toString();
 // console.log('WEBPACK: ', versionString);
 
-module.exports = {
+const config = {
   target: 'node',
   entry: {
     app: './server.js',
@@ -40,3 +40,45 @@ module.exports = {
     ],
   },
 };
+
+if (process.env.NODE_ENV === 'production') {
+  delete config.devtool;
+}
+
+module.exports = config;
+
+// module.exports = {
+//   target: 'node',
+//   entry: {
+//     app: './server.js',
+//   },
+//   output: {
+//     path: path.resolve(__dirname, 'build'),
+//     filename: 'server.build.js',
+//   },
+//   devtool: 'inline-source-map',
+//   devServer: {
+//     contentBase: './dist',
+//   },
+//   resolve: {
+//     extensions: ['.js', '.jsx'],
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['@babel/env'],
+//         },
+//       },
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         // use: ['babel-loader', 'eslint-loader'],
+//         use: ['babel-loader'],
+//       },
+//     ],
+//   },
+// };
