20c471a495763015cde39b9a8bf6f9e142f41c99
Jens Kristian Hoel <github@hoelweb.com>
2019-09-03 00:30:26 +0200

    ADD: Login endpoint

--- routes/api/users-sql.js
+++ routes/api/users-sql.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Load input validation
 const validateRegisterInput = require('../../validation/register');
-// const validateLoginInput = require('../../validation/login');
+const validateLoginInput = require('../../validation/login');
 
 // Initialize the router
 const router = express.Router();
 
 // MySQL Connection
 const sql = require('../../utils/db');
+
+// Get keys
+const keys = require('../../config/keys');
 
 /**
  * @route GET api/v1/users/test
@@ -55,6 +59,8 @@ router.post('/register', (req, res) => {
       if (_err) throw _err;
       newUser.password = hash;
 
+      console.log('Password Hash: %s', hash);
+
       // .. save user to db
       const query = `INSERT INTO users (name, password, email, avatar) VALUES ('${newUser.name}', '${newUser.password}', '${newUser.email}', '${newUser.avatar}')`;
       sql(query).then((data) => {
@@ -63,6 +69,74 @@ router.post('/register', (req, res) => {
         console.log(data);
       });
     });
+  });
+
+  return null;
+});
+
+/**
+ * @route POST api/v1/users/login
+ * @desc Login a user
+ * @access Public
+ */
+router.post('/login', (req, res) => {
+  // Check input validation
+  const { errors, isValid } = validateLoginInput({
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const { email, password } = req.body;
+
+  // Look up user by email
+  const query = `SELECT * from users WHERE email='${email}'`;
+  sql(query).then((data) => {
+    const user = data.rows[0];
+
+    // Throw error if user email is not found
+    if (!user) {
+      errors.email = 'User not found';
+      return res.status(400).json(errors);
+    }
+
+    // .. check password
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        // ... user matched! - Create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.name,
+          avatar: user.avatar,
+          roles: user.roles,
+        };
+
+        // Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 3600,
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: `Bearer ${token}`,
+            });
+          },
+        );
+      } else {
+        errors.password = 'Password incorrect';
+        return res.status(400).json(errors);
+      }
+
+      return null;
+    });
+
+    return null;
   });
 
   return null;
