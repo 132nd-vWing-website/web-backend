1f7ebd41b8a14e109577dbc3731bead2aacdf333
Jens Kristian Hoel <github@hoelweb.com>
2019-09-06 18:23:45 +0200

    ADD: Private endpoint 'users/current' added

--- routes/api/users-sql.js
+++ routes/api/users-sql.js
@@ -143,6 +143,21 @@ router.post('/login', (req, res) => {
 });
 
 /**
+ * @route GET api/v1/users/current
+ * @desc Return the current user
+ * @access Private
+ */
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email,
+    avatar: req.user.avatar,
+    roles: req.user.roles,
+  });
+});
+
+/**
  * @route GET api/v1/users/user
  * @desc Retrieves a spessific user
  * @access Private
