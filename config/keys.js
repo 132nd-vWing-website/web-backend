4ba449391d2fa4815d9c88d7acbcdfddfa179a3f
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-10-01 07:52:21 +0200

    CHANGE: Optimized for minifying

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,14 @@
 /* eslint global-require: 0 */
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys_prod');
-} else if (process.env.NODE_ENV === 'test') {
-  module.exports = require('./keys_test');
-} else {
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./keys_prod');
+// } else if (process.env.NODE_ENV === 'test') {
+//   module.exports = require('./keys_test');
+// } else {
+//   module.exports = require('./keys_dev');
+// }
+
+if (process.env.NODE_ENV !== 'production') {
   module.exports = require('./keys_dev');
+} else {
+  module.exports = require('./keys_prod');
 }
